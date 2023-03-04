1408951040cbbd2e8cb1700d05cd322c55be636d
Kostas Tsalikis <tsalikispk@gmail.com>
2023-03-05 01:30:49 +0200

    Throw error when detekt fails pre-push

--- config/kscript/pre-push-hook.kts
+++ config/kscript/pre-push-hook.kts
@@ -2,18 +2,26 @@
 import java.io.File
 
 println("Running pre-push hook")
-detekt()
+"./gradlew detekt".runWithRedirect()
 
-
-fun detekt() {
-    "./gradlew detekt".runWithRedirect()
+fun error(message: String, throwable: Throwable? = null, statusCode: Int = 1) {
+    System.err.println("❌\t${ConsoleColors.ANSI_RED}$message${ConsoleColors.ANSI_RESET}")
 }
 
-fun String.runWithRedirect(directory: File? = null): Int {
-    return ProcessBuilder("/bin/sh", "-c", this)
+fun String.runWithRedirect(directory: File? = null) {
+    val status = ProcessBuilder("/bin/sh", "-c", this)
         .redirectErrorStream(true)
         .inheritIO()
         .directory(null)
         .start()
         .waitFor()
+    if (status == 1) {
+        error("Failed running: $this")
+    }
+    System.exit(status)
+}
+
+object ConsoleColors {
+    const val ANSI_RED = "\u001B[0m"
+    const val ANSI_RESET = "\u001B[31m"
 }
