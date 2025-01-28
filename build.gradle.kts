2d6804e190ec918dc214a572068f2e7176483ada
Kostas Tsalikis <tsalikispk@gmail.com>
2025-01-29 00:48:07 +0200

    Add detekt (#1)
    * Add detekt to all modules, generate the config(./gradlew detektGenerateConfig) and auto-correct(./gradlew detekt)
    
    * Manually fix the following issues:
    * LibraryEntitiesShouldNotBePublic: disable the rule
    * WildcardImport: Manually remove wildcards in unit tests
    * MagicNumber: set ignorePropertyDeclaration to true, to allow declaring colors in Compose themes
    * FunctionNaming: Ignore functions annotated with Composable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,33 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    config.from(files(rootProject.files("config/detekt/detekt.yml")))
+    buildUponDefaultConfig = true
+    autoCorrect = true
+
+    source.from(files(projectDir))
+    tasks.withType<Detekt>().configureEach {
+        reports {
+            xml.required.set(false)
+            txt.required.set(false)
+            sarif.required.set(false)
+            html {
+                required.set(true)
+                outputLocation.set(file("build/reports/detekt.html"))
+            }
+        }
+    }
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+    detektPlugins(libs.detekt.rules)
 }
